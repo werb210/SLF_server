eae5f45ccf26fcc2ed2ea38442aaef56d2c0ce99
werb210 <todd.w@boreal.financial>
2026-03-04 11:28:35 -0700

    Add CI lint/typecheck/test scripts and ESLint setup

---
+++ eslint.config.cjs
@@ -0,0 +1,22 @@
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  },
+];
